939d1749f3e84a3d4a8f0c66e7c8f7b1e016e55f
williamsjoehenry <williamsjoehenry@gmail.com>
2024-02-09 23:51:30 -0500

    discarded avg row value idea for splitting lines

--- clean_pdfs.py
+++ clean_pdfs.py
@@ -25,14 +25,14 @@ def get_reds(image):
 
     # Iterate through each pixel
     for x in range(round(0.1*width), round(0.3*width)):
-        for y in range(round(0.1*height), round(0.2*height)):
+        for y in range(round(0.1*height), round(0.15*height)):
             pixel = image.getpixel((x, y))
             if is_approximately_red(pixel):
                 top.append(x)
 
     # Iterate through each pixel
     for x in range(round(0.1*width), round(0.3*width)):
-        for y in range(round(0.8*height), round(0.9*height)):
+        for y in range(round(0.8*height), round(0.85*height)):
             pixel = image.getpixel((x, y))
             if is_approximately_red(pixel):
                 bottom.append(x)
@@ -55,7 +55,19 @@ def rotate_page(image):
 
     return image.rotate(angle, resample = Image.BICUBIC, expand = True).show()
 
+def split_lines(image):
+    width, height = image.size
+    for row in range(height):
+        pixels = list(image.getdata())[row * width: (row + 1) * width]
+        row_avg = np.mean(pixels)
+        print(row, row_avg)
+    return row_avg
+    # img_array = np.array(image, dtype=np.uint8)
+    # return(img_array)
+
 # Example usage
-input_image_path = "data/3-92.jpg"
+input_image_path = "data/2-138.jpg"
 test_image = Image.open(input_image_path)
-rotate_page(test_image)
+width, height = test_image.size
+split_lines(test_image)
+# rotate_page(test_image)
