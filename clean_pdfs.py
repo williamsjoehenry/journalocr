ce6d3689708e95bb11cb9d821fc2ac7598941e54
williamsjoehenry <williamsjoehenry@gmail.com>
2024-02-10 00:31:13 -0500

    split pages vertically into sidebar and body

--- clean_pdfs.py
+++ clean_pdfs.py
@@ -1,4 +1,4 @@
-# from pdf2image import convert_from_path
+from pdf2image import convert_from_path
 import os
 from PIL import Image
 import numpy as np
@@ -6,17 +6,13 @@ import math
 
 os.chdir("C:/Users/pithy/Documents/journalocr")
 
-# for journal_no in [1,2,3]:
-#     images = convert_from_path(f'data/{journal_no}.pdf')
-#     for i, img in enumerate(images):
-#         img.save(f'data/{journal_no}-{i}.jpg', 'JPEG')
-
-def is_approximately_red(pixel):
+def is_red(pixel):
     # Check if pixel falls within the red color range
     r, g, b = pixel
-    return r - max(g, b) > 13
+    return r - max(g, b) > 12
 
-def get_reds(image):
+def get_reds(image_path):
+    image = Image.open(image_path).convert('RGB')
     top = []
     bottom = []
 
@@ -27,24 +23,25 @@ def get_reds(image):
     for x in range(round(0.1*width), round(0.3*width)):
         for y in range(round(0.1*height), round(0.15*height)):
             pixel = image.getpixel((x, y))
-            if is_approximately_red(pixel):
+            if is_red(pixel):
                 top.append(x)
 
     # Iterate through each pixel
     for x in range(round(0.1*width), round(0.3*width)):
         for y in range(round(0.8*height), round(0.85*height)):
             pixel = image.getpixel((x, y))
-            if is_approximately_red(pixel):
+            if is_red(pixel):
                 bottom.append(x)
     return [top, bottom]
 
-def rotate_page(image):
-    before = get_reds(image)
+def rotate_and_cleave_page(image_path):
+    image = Image.open(image_path).convert('RGB')
+    before = get_reds(image_path)
 
     top_mean = np.mean(before[0])
     bottom_mean = np.mean(before[1])
 
-    _, height = image.size
+    width, height = image.size
 
     top = 0.15*height
     bottom = 0.85*height
@@ -53,21 +50,32 @@ def rotate_page(image):
 
     angle = math.degrees(math.atan(slope)) + 90
 
-    return image.rotate(angle, resample = Image.BICUBIC, expand = True).show()
+    image = image.rotate(angle, resample = Image.BICUBIC, expand = True)
+
+    # split rotated image and save sidebar and body text
+    split_point = np.mean(get_reds(image_path)[0])
+    sidebar = image.crop(box = (0, 0, split_point, height))
+    body = image.crop(box = (split_point, 0, width, height))
+    
+    sidebar.save(f'{image_path[0:-4]}-s.jpg')
+    body.save(f'{image_path[0:-4]}-b.jpg')
+
 
 def split_lines(image):
     width, height = image.size
-    for row in range(height):
-        pixels = list(image.getdata())[row * width: (row + 1) * width]
-        row_avg = np.mean(pixels)
-        print(row, row_avg)
-    return row_avg
-    # img_array = np.array(image, dtype=np.uint8)
-    # return(img_array)
+    # I think we probably have to find the first and second lines and just use those as a scale
+    pass
+
 
 # Example usage
 input_image_path = "data/2-138.jpg"
-test_image = Image.open(input_image_path)
-width, height = test_image.size
-split_lines(test_image)
-# rotate_page(test_image)
+# test_image = Image.open(input_image_path)
+# width, height = test_image.size
+# split_lines(test_image)
+# rotate_and_cleave_page(input_image_path)
+
+for journal_no in [1,2,3]:
+    images = convert_from_path(f'data/{journal_no}.pdf')
+    for i, img in enumerate(images):
+        # TODO: consider refactoring image/path so that you don't need to save the intact pages as intermediaries?
+        img.save(f'data/{journal_no}-{i}.jpg', 'JPEG')
