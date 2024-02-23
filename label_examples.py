aba91f3400944cc197ac17a3678a1bb7417766da
williamsjoehenry <williamsjoehenry@gmail.com>
2024-02-22 22:13:19 -0500

    finished 200 training examples, let's see if that's enough

--- label_examples.py
+++ label_examples.py
@@ -4,12 +4,13 @@ from os import listdir, chdir
 
 chdir("Documents/journalocr")
 
+i = 0
 while True:
     filename = f"data/lines/{randint(1,3)}-{randint(14, 151)}-{randint(0, 20)}.jpeg"
     with Image.open(filename) as img:
         img.convert('RGB').show()
-    label = input(">")
+    label = input(f"{[i]} >")
     with open("fine_tune_examples.txt", "a") as out:
         out.write(filename+' '+label+'\n')
     out.close()
-    img.close()
+    i += 1
